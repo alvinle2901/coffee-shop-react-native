1cdb9cc0117ec0aa2b0053e4d19db5f698674492
alvinle2901 <bach938@gmail.com>
2023-10-11 15:01:04 +0300

    CustomIcon setup

--- src/theme/theme.ts
+++ src/theme/theme.ts
@@ -1,18 +1,18 @@
 interface Spacing {
-  space_2: number;
-  space_4: number;
-  space_8: number;
-  space_10: number;
-  space_12: number;
-  space_15: number;
-  space_16: number;
-  space_18: number;
-  space_20: number;
-  space_24: number;
-  space_28: number;
-  space_30: number;
-  space_32: number;
-  space_36: number;
+  space_2: number
+  space_4: number
+  space_8: number
+  space_10: number
+  space_12: number
+  space_15: number
+  space_16: number
+  space_18: number
+  space_20: number
+  space_24: number
+  space_28: number
+  space_30: number
+  space_32: number
+  space_36: number
 }
 
 export const SPACING: Spacing = {
@@ -29,22 +29,22 @@ export const SPACING: Spacing = {
   space_28: 28,
   space_30: 30,
   space_32: 32,
-  space_36: 36,
-};
+  space_36: 36
+}
 
 interface Color {
-  primaryRedHex: string;
-  primaryOrangeHex: string;
-  primaryBlackHex: string;
-  primaryDarkGreyHex: string;
-  secondaryDarkGreyHex: string;
-  primaryGreyHex: string;
-  secondaryGreyHex: string;
-  primaryLightGreyHex: string;
-  secondaryLightGreyHex: string;
-  primaryWhiteHex: string;
-  primaryBlackRGBA: string;
-  secondaryBlackRGBA: string;
+  primaryRedHex: string
+  primaryOrangeHex: string
+  primaryBlackHex: string
+  primaryDarkGreyHex: string
+  secondaryDarkGreyHex: string
+  primaryGreyHex: string
+  secondaryGreyHex: string
+  primaryLightGreyHex: string
+  secondaryLightGreyHex: string
+  primaryWhiteHex: string
+  primaryBlackRGBA: string
+  secondaryBlackRGBA: string
 }
 
 export const COLORS: Color = {
@@ -59,19 +59,19 @@ export const COLORS: Color = {
   secondaryLightGreyHex: '#AEAEAE',
   primaryWhiteHex: '#FFFFFF',
   primaryBlackRGBA: 'rgba(12,15,20,0.5)',
-  secondaryBlackRGBA: 'rgba(0,0,0,0.7)',
-};
+  secondaryBlackRGBA: 'rgba(0,0,0,0.7)'
+}
 
 interface FontFamily {
-  poppins_black: string;
-  poppins_bold: string;
-  poppins_extrabold: string;
-  poppins_extralight: string;
-  poppins_light: string;
-  poppins_medium: string;
-  poppins_regular: string;
-  poppins_semibold: string;
-  poppins_thin: string;
+  poppins_black: string
+  poppins_bold: string
+  poppins_extrabold: string
+  poppins_extralight: string
+  poppins_light: string
+  poppins_medium: string
+  poppins_regular: string
+  poppins_semibold: string
+  poppins_thin: string
 }
 
 export const FONTFAMILY: FontFamily = {
@@ -83,20 +83,20 @@ export const FONTFAMILY: FontFamily = {
   poppins_medium: 'Poppins-Medium',
   poppins_regular: 'Poppins-Regular',
   poppins_semibold: 'Poppins-SemiBold',
-  poppins_thin: 'Poppins-Thin',
-};
+  poppins_thin: 'Poppins-Thin'
+}
 
 interface FontSize {
-  size_8: number;
-  size_10: number;
-  size_12: number;
-  size_14: number;
-  size_16: number;
-  size_18: number;
-  size_20: number;
-  size_24: number;
-  size_28: number;
-  size_30: number;
+  size_8: number
+  size_10: number
+  size_12: number
+  size_14: number
+  size_16: number
+  size_18: number
+  size_20: number
+  size_24: number
+  size_28: number
+  size_30: number
 }
 
 export const FONTSIZE: FontSize = {
@@ -109,16 +109,16 @@ export const FONTSIZE: FontSize = {
   size_20: 20,
   size_24: 24,
   size_28: 28,
-  size_30: 30,
-};
+  size_30: 30
+}
 
 interface BorderRadius {
-  radius_4: number;
-  radius_8: number;
-  radius_10: number;
-  radius_15: number;
-  radius_20: number;
-  radius_25: number;
+  radius_4: number
+  radius_8: number
+  radius_10: number
+  radius_15: number
+  radius_20: number
+  radius_25: number
 }
 
 export const BORDERRADIUS: BorderRadius = {
@@ -127,5 +127,5 @@ export const BORDERRADIUS: BorderRadius = {
   radius_10: 10,
   radius_15: 15,
   radius_20: 20,
-  radius_25: 25,
-};
+  radius_25: 25
+}
