1066380d64bfc27a102901b50723c96a94121c9f
alvinle2901 <bach938@gmail.com>
2023-10-11 17:53:42 +0300

    config store zustand

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,7 +1,12 @@
-import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+import { useStore } from '../store/store'
 
 const HomeScreen = () => {
+  const CoffeeList = useStore((state: any) => state.CoffeeList)
+  const BeanList = useStore((state: any) => state.BeanList)
+
+
   return (
     <View>
       <Text>HomeScreen</Text>
