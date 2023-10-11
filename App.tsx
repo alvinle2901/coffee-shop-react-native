ef3389e61df3c61cf1bd86de5441da2529d3c63b
alvinle2901 <bach938@gmail.com>
2023-10-11 15:13:37 +0300

    added Stack Navigation

--- App.tsx
+++ App.tsx
@@ -1,14 +1,31 @@
-import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+
+import { createNativeStackNavigator } from '@react-navigation/native-stack'
+import { NavigationContainer } from '@react-navigation/native'
+
+import HomeScreen from './src/screens/HomeScreen'
+import PaymentScreen from './src/screens/PaymentScreen'
+import DetailScreen from './src/screens/DetailScreen'
+
+const Stack = createNativeStackNavigator()
 
 const App = () => {
   return (
-    <View>
-      <Text>App</Text>
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="Detail"
+          component={DetailScreen}
+          options={{ animation: 'slide_from_bottom' }}
+        ></Stack.Screen>
+        <Stack.Screen
+          name="Payment"
+          component={PaymentScreen}
+          options={{ animation: 'slide_from_bottom' }}
+        ></Stack.Screen>
+      </Stack.Navigator>
+    </NavigationContainer>
   )
 }
 
 export default App
-
-const styles = StyleSheet.create({})
