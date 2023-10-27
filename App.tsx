51ee61ff97fe0941e815e9ef8aa11c13da7afc2e
alvinle2901 <bach938@gmail.com>
2023-10-28 00:16:09 +0300

    install dependencies for Stripe

--- App.tsx
+++ App.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 
 import { createNativeStackNavigator } from '@react-navigation/native-stack'
 import { NavigationContainer } from '@react-navigation/native'
+import { StripeProvider } from '@stripe/stripe-react-native'
 
 import PaymentScreen from './src/screens/PaymentScreen'
 import DetailScreen from './src/screens/DetailScreen'
@@ -11,25 +12,30 @@ const Stack = createNativeStackNavigator()
 
 const App = () => {
   return (
-    <NavigationContainer>
-      <Stack.Navigator screenOptions={{ headerShown: false }}>
-        <Stack.Screen
-          name="TabNavigation"
-          component={TabNavigation}
-          options={{ animation: 'slide_from_bottom' }}
-        ></Stack.Screen>
-        <Stack.Screen
-          name="Detail"
-          component={DetailScreen}
-          options={{ animation: 'slide_from_bottom' }}
-        ></Stack.Screen>
-        <Stack.Screen
-          name="Payment"
-          component={PaymentScreen}
-          options={{ animation: 'slide_from_bottom' }}
-        ></Stack.Screen>
-      </Stack.Navigator>
-    </NavigationContainer>
+    <StripeProvider
+      publishableKey="pk_test_51O5urOD8OS8hthp2jKKaiT1pQTleoDcvG9AUTaECcm1ZAHrfhMAvmz9rRpKO8Lzpn1H1FcWnTAl4sNpsR83nqBIT00Enxyhat1"
+      // urlScheme="your-url-scheme"
+    >
+      <NavigationContainer>
+        <Stack.Navigator screenOptions={{ headerShown: false }}>
+          <Stack.Screen
+            name="TabNavigation"
+            component={TabNavigation}
+            options={{ animation: 'slide_from_bottom' }}
+          ></Stack.Screen>
+          <Stack.Screen
+            name="Detail"
+            component={DetailScreen}
+            options={{ animation: 'slide_from_bottom' }}
+          ></Stack.Screen>
+          <Stack.Screen
+            name="Payment"
+            component={PaymentScreen}
+            options={{ animation: 'slide_from_bottom' }}
+          ></Stack.Screen>
+        </Stack.Navigator>
+      </NavigationContainer>
+    </StripeProvider>
   )
 }
 
